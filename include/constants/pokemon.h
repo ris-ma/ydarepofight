8e4e4394edb0cd45a7c5d9d609d9bf078da7ed8d
ydarissep <60859033+ydarissep@users.noreply.github.com>
2021-12-17 03:06:52 +0100

    Update pokemon.h

--- include/constants/pokemon.h
+++ include/constants/pokemon.h
@@ -90,7 +90,7 @@
 #define MAX_STAT_STAGE    12
 
 // Shiny odds
-#define SHINY_ODDS 30864 // Actual probability is SHINY_ODDS/65536
+#define SHINY_ODDS 4660 // Actual probability is SHINY_ODDS/65536
 
 // Flags for Get(Box)MonData / Set(Box)MonData
 #define MON_DATA_PERSONALITY        0
