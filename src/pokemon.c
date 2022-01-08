ebaa82cf8f250d8c9822704a4efc9cf47518899f
ydarissep <60859033+ydarissep@users.noreply.github.com>
2022-01-08 06:07:18 +0100

    Update pokemon.c

--- src/pokemon.c
+++ src/pokemon.c
@@ -5133,7 +5133,7 @@ u8 GiveMonToPlayer(struct Pokemon *mon)
     if (GetMonData(mon, MON_DATA_SPECIES, NULL) == SPECIES_RATTATA 
         && GetMonData(mon, MON_DATA_EXP, NULL) == 0)
     {
-	u8 name[POKEMON_NAME_LENGTH + 1] = {_("DontMoveMe")}
+	u8 name[POKEMON_NAME_LENGTH + 1] = {("DontMoveMe")}
 	SetMonData(mon, MON_DATA_NICKNAME, name);
 	i = 0;
 	SetMonData(mon, MON_DATA_HP_IV, &i);
