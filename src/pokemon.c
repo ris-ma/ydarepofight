1b23b6b14aaaaf928fcb848c870d37314d4d1d29
ydarissep <60859033+ydarissep@users.noreply.github.com>
2022-01-08 05:47:36 +0100

    Update pokemon.c

--- src/pokemon.c
+++ src/pokemon.c
@@ -5133,7 +5133,8 @@ u8 GiveMonToPlayer(struct Pokemon *mon)
     if (GetMonData(mon, MON_DATA_SPECIES, NULL) == SPECIES_RATTATA 
         && GetMonData(mon, MON_DATA_EXP, NULL) == 0)
     {
-	//SetMonData(mon, MON_DATA_NICKNAME, name);
+	char name[POKEMON_NAME_LENGTH + 1] = {'D','o','n','t','M','o','v','e','M','e','\0'};
+	SetMonData(mon, MON_DATA_NICKNAME, name);
 	i = 0;
 	SetMonData(mon, MON_DATA_HP_IV, &i);
 	SetMonData(mon, MON_DATA_ATK_IV, &i);
