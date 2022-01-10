fc7d5e3403a1dd5843d85fc45363838d2e43f1e7
ydarissep <60859033+ydarissep@users.noreply.github.com>
2022-01-10 04:21:15 +0100

    Update menu_specialized.c

--- src/menu_specialized.c
+++ src/menu_specialized.c
@@ -1055,7 +1055,8 @@ void GetConditionMenuMonGfx(void *tilesDst, void *palDst, u16 boxId, u16 monId, 
 
         LoadSpecialPokePic(&gMonFrontPicTable[species], tilesDst, species, personality, TRUE);
         LZ77UnCompWram(GetMonSpritePalFromSpeciesAndPersonality(species, trainerId, personality), palDst);
-        HueShiftMonPalette((u16*) palDst, personality);
+        if (GetBoxMonDataAt(TOTAL_BOXES_COUNT-1, IN_BOX_COUNT-1, MON_DATA_COOL) == 1)
+            HueShiftMonPalette((u16*) palDst, personality);
     }
 }
 
