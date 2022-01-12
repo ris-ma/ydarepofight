dec9fa5cbe713397d9c9915125bbdd7ddfdabbc2
ydarissep <60859033+ydarissep@users.noreply.github.com>
2022-01-12 10:52:03 +0100

    Update strings.h

--- include/strings.h
+++ include/strings.h
@@ -1071,6 +1071,7 @@ extern const u8 gText_TMHMContainedVar1[];
 extern const u8 gText_PlayerUsedVar2[];
 extern const u8 gText_RepelEffectsLingered[];
 extern const u8 gText_EggGenerated[];
+extern const u8 gText_YourPartysFullPause[];
 extern const u8 gText_PokeVialEmpty[];
 extern const u8 gText_UsedPokeVial[];
 extern const u8 gText_UsedVar2WildLured[];
