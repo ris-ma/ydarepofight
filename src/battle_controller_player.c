08807bc2283d30228c6e699948a2fce2131d2cd9
ydarissep <60859033+ydarissep@users.noreply.github.com>
2022-01-18 13:49:37 +0100

    Update battle_controller_player.c

--- src/battle_controller_player.c
+++ src/battle_controller_player.c
@@ -268,6 +268,13 @@ static void HandleInputChooseAction(void)
         }
         PlayerBufferExecCompleted();
     }
+    else if (JOY_NEW(L_BUTTON))
+    {
+        gBattlerControllerFuncs[gActiveBattler] = CompleteOnBattlerSpriteCallbackDummy;
+        BeginNormalPaletteFade(0xFFFFFFFF, 0, 0, 0x10, RGB_BLACK);
+        FreeAllWindowBuffers();
+        ShowPokemonSummaryScreen(SUMMARY_MODE_LOCK_MOVES, gEnemyParty, 0, CalculateEnemyPartyCount() - 1, CB2_SetUpReshowBattleScreenAfterMenu);
+    }
     else if (JOY_NEW(DPAD_LEFT))
     {
         if (gActionSelectionCursor[gActiveBattler] & 1) // if is B_ACTION_USE_ITEM or B_ACTION_RUN
