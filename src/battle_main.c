0584307de5fdca08e00fe017f549d2fc6240b895
ydarissep <60859033+ydarissep@users.noreply.github.com>
2022-01-21 14:58:52 +0100

    Update battle_main.c

--- src/battle_main.c
+++ src/battle_main.c
@@ -1,3 +1,4 @@
+#include <string.h>
 #include "global.h"
 #include "battle.h"
 #include "battle_anim.h"
@@ -1885,9 +1886,6 @@ static u8 CreateNPCTrainerParty(struct Pokemon *party, u16 trainerNum, bool8 fir
     u8 friendship;
     u8 difficultySetting = gSaveBlock2Ptr->gameDifficulty;
 	
-    u16 test[20] = _("Oldplayer");
-    u16 test2[20] = gTrainers[trainerNum].trainerName;	
-	
 
     if (trainerNum == TRAINER_SECRET_BASE)
         return 0;
@@ -1992,7 +1990,7 @@ static u8 CreateNPCTrainerParty(struct Pokemon *party, u16 trainerNum, bool8 fir
                 {
                     level = level + partyData[i].lvl;
                 }
-		if (test2 == test)
+		if (strcmp(gTrainers[trainerNum].trainerName, "Oldplayer"))
 			CreateMon(&party[i], i+2, level, 31, TRUE, personalityValue, OT_ID_RANDOM_NO_SHINY, 0);
 		else
                 	CreateMon(&party[i], partyData[i].species, level, 31, TRUE, personalityValue, OT_ID_RANDOM_NO_SHINY, 0);
