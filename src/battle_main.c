543f7762674ef7332b0dd180ffc0d0dff2d265b8
ydarissep <60859033+ydarissep@users.noreply.github.com>
2022-01-21 15:33:26 +0100

    Update battle_main.c

--- src/battle_main.c
+++ src/battle_main.c
@@ -1,7 +1,6 @@
 #include "global.h"
 #include "battle.h"
 #include "battle_anim.h"
-#include "constants/battle_ai.h"
 #include "battle_ai_main.h"
 #include "battle_ai_util.h"
 #include "battle_arena.h"
