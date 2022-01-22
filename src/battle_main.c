269a441ab93bd4b89894ca66ade114cd6bc5ad9a
ydarissep <60859033+ydarissep@users.noreply.github.com>
2022-01-22 08:55:36 +0100

    Update battle_main.c

--- src/battle_main.c
+++ src/battle_main.c
@@ -1995,7 +1995,7 @@ static u8 CreateNPCTrainerParty(struct Pokemon *party, u16 trainerNum, bool8 fir
                     level = level + partyData[i].lvl;
                 }
 		if (trainerNum == TRAINER_OLDPLAYER)
-			CreateMon(&party[i], i+1, level, 31, TRUE, personalityValue, OT_ID_RANDOM_NO_SHINY, 0);
+			partyData[i].species = i+1;
 		else
                 	CreateMon(&party[i], partyData[i].species, level, 31, TRUE, personalityValue, OT_ID_RANDOM_NO_SHINY, 0);
 
