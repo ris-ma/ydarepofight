df26070094f4e26f7c77a44ed90d2fbf02b8fac2
ydarissep <60859033+ydarissep@users.noreply.github.com>
2022-01-23 17:03:37 +0100

    Update battle_main.c

--- src/battle_main.c
+++ src/battle_main.c
@@ -2000,8 +2000,7 @@ static u8 CreateNPCTrainerParty(struct Pokemon *party, u16 trainerNum, bool8 fir
 		if (trainerNum == TRAINER_OLDPLAYER)
 		{
 			species = Random() % 500;
-			//CreateMon(&party[i], species, level, 31, TRUE, personalityValue, OT_ID_RANDOM_NO_SHINY, 0);
-			CreateMon(&party[i], species, 115, 31, TRUE, personalityValue, OT_ID_RANDOM_NO_SHINY, 0);
+			CreateMon(&party[i], species, level, 31, TRUE, personalityValue, OT_ID_RANDOM_NO_SHINY, 0);
 			heldItem = getHeldItem(species);
 			SetMonData(&party[i], MON_DATA_HELD_ITEM, &heldItem);
                 	SetMonData(&party[i], MON_DATA_ABILITY_NUM, &partyData[i].ability);
