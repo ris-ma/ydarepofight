1a981abaadaf20663ccc53ed83a66707bd3462d3
ydarissep <60859033+ydarissep@users.noreply.github.com>
2022-01-24 07:20:20 +0100

    Update pokemon.c

--- src/pokemon.c
+++ src/pokemon.c
@@ -3882,7 +3882,7 @@ void CalculateMonStats(struct Pokemon *mon)
     s32 level = GetLevelFromMonExp(mon);
     s32 newMaxHP;
 
-	level = GetMonData(mon, MON_DATA_LEVEL, NULL)
+	level = GetMonData(mon, MON_DATA_LEVEL, NULL);
     SetMonData(mon, MON_DATA_LEVEL, &level);
 
     if (species == SPECIES_SHEDINJA)
